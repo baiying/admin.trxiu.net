db95e01422b9d5157e6fc0070509405f6147197b
Maple <crazy.maple@foxmail.com>
2016-06-12 16:24:39 +0800

    添加主播管理文件

--- controllers/AnchorController.php
+++ controllers/AnchorController.php
@@ -11,7 +11,33 @@ class AnchorController extends BaseController {
     public $layout = "main";
 
     public function actionIndex() {
-        return $this->render('index');
+        $this->js[] = "/js/system/manager.js";
+        $this->css[] = "/media/css/DT_bootstrap.css";
+
+        $renderArgs = [];
+        // 处理传入参数
+        $rule = [
+            'page'      => ['type'=>'int', 'default'=>1],
+            'pagesize'  => ['type'=>'int', 'default'=>2],
+            'order'     => ['type'=>'string', 'default'=>'manager_id DESC']
+        ];
+        $args = $this->getRequestData($rule, Yii::$app->request->get());
+
+        // 获取管理员账号信息
+        $res = Yii::$app->api->get('manager/search', $args);
+        if($res['code'] != 200) {
+            $renderArgs['error'] = $res['message'];
+            return $this->render('/site/error', $renderArgs);
+        }
+        // 将管理员列表数据放入renderArgs数组
+        $renderArgs['managers'] = $res['data'];
+        // 生成翻页HTML
+        $pageUrl = '/system/manager/?page=$page';
+        $maxPage = ceil($res['count'] / $args['pagesize']);
+        $renderArgs['pageBar'] = Yii::$app->utils->getPaging($pageUrl, $args['page'], $maxPage);
+
+//        return $this->render('manager', $renderArgs);
+        return $this->render('index', $renderArgs);
     }
 
 }
