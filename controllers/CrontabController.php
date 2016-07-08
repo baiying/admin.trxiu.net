0873c34cb8f0c14d11374bcd591214e99491e9eb
evan.bai <cn.baiying@gmail.com>
2016-07-08 10:59:48 +0800

    更新计划任务

--- controllers/CrontabController.php
+++ controllers/CrontabController.php
@@ -10,12 +10,12 @@ class CrontabController extends Controller {
      * 更新活动状态
      */
     public function actionCronBallotStatus() {
-        Yii::$app->api->post('ballot/change-status');
+        Yii::$app->api->get('ballot/change-status');
     }
     /**
      * send-red-package
      * 发送拉票红包
-     * 发送规则：每5秒钟选择一个最早领取的红包进行发送
+     * 发送规则：每10秒钟选择一个最早领取的红包进行发送
      */
     public function actionSendRedPackage() {
         Yii::$app->api->get('canvass/send-red-package');
